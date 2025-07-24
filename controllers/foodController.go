d1e56cdcfc9fd0d85e628da9df560c2428403293
ezParth <parthsingh55m@gmail.com>
2025-07-24 12:12:10 +0530

    models standings

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,6 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"restro_backend/database"
+	"time"
+
+	"retro_backend/models"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -10,7 +23,17 @@ func GetFoods() gin.HandlerFunc {
 
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food"})
+		}
+
+		c.JSON(http.StatusOK, food)
 	}
 }
 
