4111772e9d7912452c08da1b8f651bc8255a5225
ezParth <parthsingh55m@gmail.com>
2025-07-29 23:50:27 +0530

    added getorders and getorder controller

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,16 +1,47 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"restro_backend/database"
+	"restro_backend/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items"})
+		}
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allOrders)
 	}
 }
 
 func GetOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderId := c.Param("order_id")
+		var order models.Order
+		err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, order)
 	}
 }
 
